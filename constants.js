6e412bfcd3191eab2e41e6289f52eed57bd2ddf9
rvenegas5 <rvenegas@espol.edu.ec>
2022-01-16 15:44:06 -0500

    Server Settings

---
+++ constants.js
@@ -0,0 +1,17 @@
+module.exports = {
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+    MYSQL_USER: process.env.MYSQL_USER,
+    MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD,
+    MYSQL_HOSTNAME: process.env.MYSQL_HOSTNAME,
+    MYSQL_ROOT_PORT: process.env.MYSQL_ROOT_PORT,
+    ROL: {
+      CLIENTE: {
+        ROL_ID: 1,
+        ROL: "ADMINISTRADOR_FUNDACION",
+      },
+      ABOGADO: {
+        ROL_ID: 2,
+        ROL: "DONADOR",
+      },
+    },
+  };
