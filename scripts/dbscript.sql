64292323020596c36f08a1543c7266a61d8757de
rvenegas5 <rvenegas@espol.edu.ec>
2022-01-16 16:42:33 -0500

    DB

---
+++ scripts/dbscript.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS abonet;
+CREATE DATABASE abonet;
+USE abonet;
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario`(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `cedula` varchar(10) NOT NULL,
+    `nombres` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `apellidos` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `telefono` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `direccion` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `usuario` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `password` varchar(191) NOT NULL,
+    `role` int(1) NOT NULL,
+	`createdAt` datetime DEFAULT NULL,
+	`updatedAt` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `abogado`;
+CREATE TABLE `abogado`(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `id_usuario`int(11) NOT NULL,
+    `linkedin` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `especializacion` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `bufete` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+	`createdAt` datetime DEFAULT NULL,
+	`updatedAt` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS `calificacion`;
+CREATE TABLE `calificacion`(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `id_usuario`int(11) NOT NULL,
+    `estrellas` DECIMAL NOT NULL,
+	`createdAt` datetime DEFAULT NULL,
+    `updatedAt` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON UPDATE CASCADE
+);
