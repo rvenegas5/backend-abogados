d7c8049450954755604e2f79853bcc94eecb4a86
rvenegas5 <rvenegas@espol.edu.ec>
2022-01-23 20:26:58 -0500

    Render array of lawyers

--- frontend_abonet/lib/home_abogados.dart
+++ frontend_abonet/lib/home_abogados.dart
@@ -143,49 +143,6 @@ class _MostrarAbogadoState extends State<MostrarAbogado> {
   }
 }
 
-class WidgetAbogado extends StatelessWidget {
-  const WidgetAbogado({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.white70,
-                minRadius: 60.0,
-                child: CircleAvatar(
-                  radius: 50.0,
-                ),
-              ),
-              title: Text('The Enchanted Nightingale'),
-              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('BUY TICKETS'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 Future<List<Abogado>> _getAbogados() async {
   List<Abogado> listaAbogados = [];
 
